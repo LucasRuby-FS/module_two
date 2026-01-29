c7178f1e8cdd39539862a85aa2a8f4d52ab47fe6
LucasRuby-FS <rklucas@student.fullsail.edu>
2026-01-29 18:35:10 -0500

    messing around before resstructuring.

--- app/db/config.js
+++ app/db/config.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+//const User = require("../models/user");
 const connectDB = async () => {
   try {
     console.log("Mongo URI:", process.env.MONGODB_URI);
@@ -9,5 +9,11 @@ const connectDB = async () => {
     console.log(error);
   }
 };
-
+/*const saveUser = async (newUser) => {
+  return await userUser.save();
+};
+const findUser = async (object) => {
+  return await User.find(object).exec();
+}*/
 module.exports = connectDB;
+//module.exports = { connectDB, saveUser, findUser };
